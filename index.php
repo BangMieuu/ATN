57edd0495203bd93ee9878b7b8592ea5dd2940e3
Bùi Khánh Toàn <52988854+BangMieuu@users.noreply.github.com>
2019-08-26 11:51:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <body>
 
  <!-- Sidebar menu -->
-<nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display;z-index:2;width:50%;min-width:300px" id="mySidebar">
+<nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:50%;min-width:300px" id="mySidebar">
   <a href="javascript:void(0)" onclick="w3_close()"
   class="w3-bar-item w3-button">Home</a>
   <a href= "login.php" onclick="w3_close()" class="w3-bar-item w3-button">Login</a>
