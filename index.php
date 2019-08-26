4118e7d875a75fcac3162c4a4d505b13d009962f
Bùi Khánh Toàn <52988854+BangMieuu@users.noreply.github.com>
2019-08-26 10:43:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -44,22 +44,28 @@ function w3_close() {
       <img src="C:\xampp\htdocs\atn\Toys\Teddy.jpg" alt="Teddy" style="width:100%">
 	<br>
 	<br>
-          <a> The most comfortable stuffed teddy bear of all time
+	<a> The most comfortable stuffed teddy bear of all time </a>
       <p> 250.000 VND </p>
     </div>
 <div class="w3-quarter">
       <img src="C:\xampp\htdocs\atn\Toys\R2D2.jpg" alt="Robot" style="width:115%">
 	<br>
 	<br>
-	<a> Everyone like Starwar series so why not have a robot 
+	<a> Everyone like Starwar series so why not having this R2D2 </a>
       <p> 650.000 VND</p>
     </div>
 <div class="w3-quarter">
       <img src="C:\xampp\htdocs\atn\Toys\ThanosPop.jpg" alt="Figure" style="width:115%">
+	<br>
+	<br>
+	<a> The most favorite figure brand in this world, featuring Thanos from the Avenger End Game series </a>
     <p> 160.000 VND</p>
     </div>
 <div class="w3-quarter">
       <img src="C:\xampp\htdocs\atn\Toys\ThanosGaunt.jpg" alt="Gauntlet" style="width:115%">
+	<br>
+	<br>
+	<a> The one and only item that can wiped all the planet in a single snap ! Why not give it a try ? </a>
       <p>6.500.000 VND </p>
     </div>
 </body>
