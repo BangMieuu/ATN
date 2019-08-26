5aa352c8bb52916f343d59f377051a1b7b794974
Bùi Khánh Toàn <52988854+BangMieuu@users.noreply.github.com>
2019-08-26 13:12:24 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,37 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/style2.css">
+    <link rel="stylesheet" href="/style.css">
     <title>Add</title>
 </head>
 
 <body>
     <div class="content">
         <h1>Product management form</h1>
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Descrip</th>
-            </tr>
-            <tr>
-                <?php
-            require_once './database.php';
-            $sql = "SELECT * FROM product";
-            $stmt = $pdo->prepare($sql);
-            foreach ($pdo->query($sql) as $row) {
-            ?>
-            </tr>
-            <tr>
-               <?php 
-        require("dbconnector.php");
+        <?php 
+        require("dbconnector.php");   
         if(isset($_POST["submit"]))
             {
-
                 $name = $_POST["proname"];
                 $price = $_POST["price"];
                 $descrip = $_POST["descrip"];
@@ -51,7 +34,7 @@
                         {
                         ?> 
                             <script>
-                                alert("Product already available!!");
+                                alert("The product already available!!");
                             </script>
                         <?php
                         }
@@ -63,21 +46,21 @@
                                 <script>
                                     alert("Added successful!");
                                     window.location.href = "index.php";
-                                </script> 
-            </tr>
-        </table>
-        ?>
-        <form action="add.php" method="POST">
+                                </script>
+                            <?php
+                        }
+                    }
+            }
+             ?>
+        <form action="/add.php" method="POST">
             <input class="input-information" type="text" width="300" height="100" name="proname" placeholder="Name"> <br>
             <input class="input-information" type="text" width="300" height="100" name="price" placeholder="Price"> <br>
             <input class="input-information" type="text" width="300" height="100" name="descrip" placeholder="Description"> <br>
             <button type="submit" value="Add" name="submit">Add</button>
         </form>
         
-        <button><a href="index.php">Back</a></button>
+        <button><a href="/index.php">Back</a></button>
     </div>
-
-
 </body>
 
 </html>
