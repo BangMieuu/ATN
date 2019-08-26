d17e51ed22ded185b50d3651b8e4a104710794ab
Bùi Khánh Toàn <52988854+BangMieuu@users.noreply.github.com>
2019-08-26 11:40:04 +0700

    Add files via upload

---
+++ dbconnector.php
@@ -0,0 +1,5 @@
+<?php
+$conn = pg_connect("host=ec2-23-23-228-132.compute-1.amazonaws.com port=5432 dbname=d4giprqaqg6clu 
+user=gwesupjtdpzeok password=5804ce399973fac0b422582b7421f10a88f5fb2cbc0d2c8f04ac28d3cdf69556") 
+or die("Connect Unsuccessful");
+?>
